2a34522f3a008de2a48019db2ce178a3d2966edf
Isaac Alexander <Isaacalexander81@gmail.com>
2026-03-22 02:25:36 +0100

    src/main/java/com/alex/student_management_system/StudentManagementSystemApplication.java

--- src/main/java/com/alex/student_management_system/StudentManagementSystemApplication.java
+++ src/main/java/com/alex/student_management_system/StudentManagementSystemApplication.java
@@ -1,21 +1,37 @@
 package com.alex.student_management_system;
 
+import com.alex.student_management_system.entity.Student;
+import com.alex.student_management_system.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementSystemApplication.class, args);
+        SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 
-    @RequestMapping
-    public String hello() {
-        return "Hello World";
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+//        Student student1 = new Student("Frank","Martins","frank@gmail.com");
+//        studentRepository.save(student1);
+//
+//        Student student2 = new Student("John","Jones","john@gmail.com");
+//        studentRepository.save(student2);
+//
+//        Student student3 = new Student("Anita","Collins","anita@gmail.com");
+//        studentRepository.save(student3);
+
+
     }
 }
