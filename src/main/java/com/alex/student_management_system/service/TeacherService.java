629c6084d303bb54fefacf17214bcb5a91d7b726
Isaac Alexander <Isaacalexander81@gmail.com>
2026-03-24 01:57:23 +0100

    src/main/java/com/alex/student_management_system/service/TeacherService.java

--- src/main/java/com/alex/student_management_system/service/TeacherService.java
+++ src/main/java/com/alex/student_management_system/service/TeacherService.java
@@ -1,4 +1,17 @@
 package com.alex.student_management_system.service;
 
+import com.alex.student_management_system.entity.Teacher;
+
+import java.util.List;
+
 public interface TeacherService {
+    List<Teacher> getAllTeachers();
+
+    Teacher saveTeacher(Teacher teacher);
+
+    Teacher getTeacherById(Long id);
+
+    Teacher updateTeacher(Teacher teacher);
+
+    void deleteTeacherById(Long id);
 }
