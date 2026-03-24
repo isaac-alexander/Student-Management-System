624517ee33144ca08fb40996c99c5f1c30195759
Isaac Alexander <Isaacalexander81@gmail.com>
2026-03-24 01:57:36 +0100

    src/main/java/com/alex/student_management_system/StudentManagementSystemApplication.java

--- src/main/java/com/alex/student_management_system/StudentManagementSystemApplication.java
+++ src/main/java/com/alex/student_management_system/StudentManagementSystemApplication.java
@@ -1,24 +1,27 @@
 package com.alex.student_management_system;
 
 import com.alex.student_management_system.entity.Student;
+import com.alex.student_management_system.entity.Teacher;
 import com.alex.student_management_system.repository.StudentRepository;
+import com.alex.student_management_system.repository.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
 @SpringBootApplication
 public class StudentManagementSystemApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
-	}
+    }
 
     @Autowired
     private StudentRepository studentRepository;
+
+    @Autowired
+    private TeacherRepository teacherRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -32,6 +35,13 @@ public class StudentManagementSystemApplication implements CommandLineRunner {
 //        Student student3 = new Student("Anita","Collins","anita@gmail.com");
 //        studentRepository.save(student3);
 
-
+//        Teacher teacher1 = new Teacher("Angela", "Stones", "angela@gmail.com", "HTML");
+//        teacherRepository.save(teacher1);
+//
+//        Teacher teacher2 = new Teacher("Kayode", "Godwin", "godwin@gmail.com", "CSS");
+//        teacherRepository.save(teacher2);
+//
+//        Teacher teacher3 = new Teacher("Emmanuel", "Charlie", "emma@gmail.com", "Java");
+//        teacherRepository.save(teacher3);
     }
 }
