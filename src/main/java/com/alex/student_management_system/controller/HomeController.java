ef1393fe5447608bf32fff6d5e21ce9e1752cf9b
Isaac Alexander <Isaacalexander81@gmail.com>
2026-03-24 01:58:44 +0100

    src/main/java/com/alex/student_management_system/controller/HomeController.java

--- src/main/java/com/alex/student_management_system/controller/HomeController.java
+++ src/main/java/com/alex/student_management_system/controller/HomeController.java
@@ -1,4 +1,14 @@
 package com.alex.student_management_system.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    // Home route
+    @GetMapping("/")
+    public String viewHomePage() {
+        return "index"; // loads index.html
+    }
 }
