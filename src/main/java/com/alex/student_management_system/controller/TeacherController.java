51da076289c25f5da93711c2cbb32f1699792fa7
Isaac Alexander <Isaacalexander81@gmail.com>
2026-03-24 01:56:27 +0100

    src/main/java/com/alex/student_management_system/controller/TeacherController.java

--- src/main/java/com/alex/student_management_system/controller/TeacherController.java
+++ src/main/java/com/alex/student_management_system/controller/TeacherController.java
@@ -1,4 +1,72 @@
 package com.alex.student_management_system.controller;
 
+import com.alex.student_management_system.entity.Teacher;
+import com.alex.student_management_system.service.TeacherService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class TeacherController {
+
+    private TeacherService teacherService;
+
+    public TeacherController(TeacherService teacherService) {
+        super();
+        this.teacherService = teacherService;
+    }
+
+    // handler method to handle listTeacher and return model and view
+
+    @GetMapping("/teachers")
+    public String listTeacher(Model model) {
+        model.addAttribute("teachers", teacherService.getAllTeachers());
+        return "teachers";
+    }
+
+    @GetMapping("/teachers/new")
+    public String createTeacherForm(Model model) {
+        // create teacher object to hold teacher form data
+        Teacher teacher = new Teacher();
+        model.addAttribute("teacher", teacher);
+        return "create_teacher"; // view/html
+    }
+
+    @PostMapping("/teachers")
+    public String saveTeacher(@ModelAttribute("teacher") Teacher teacher) {
+        teacherService.saveTeacher(teacher);
+        return "redirect:/teachers";
+    }
+
+    @GetMapping("/teachers/edit/{id}")
+    public String editTeacherForm(@PathVariable Long id, Model model) {
+        model.addAttribute("teacher", teacherService.getTeacherById(id));
+        return "edit_teacher";
+    }
+
+    @PostMapping("/teachers/{id}")
+    public String updateTeacher(@PathVariable Long id, @ModelAttribute("teacher") Teacher teacher, Model model) {
+        // get teacher from database by id
+        Teacher existingTeacher = teacherService.getTeacherById(id);
+        existingTeacher.setId(id);
+        existingTeacher.setFirstName(teacher.getFirstName());
+        existingTeacher.setLastName(teacher.getLastName());
+        existingTeacher.setEmail(teacher.getEmail());
+        existingTeacher.setCourse(teacher.getCourse());
+
+        // save updated teacher object
+        teacherService.updateTeacher(existingTeacher);
+        return "redirect:/teachers";
+    }
+
+    // handler method to handle delete teacher request
+
+    @GetMapping("/teachers/{id}")
+    public String deleteTeacher(@PathVariable Long id) {
+        teacherService.deleteTeacherById(id);
+        return "redirect:/teachers";
+    }
 }
