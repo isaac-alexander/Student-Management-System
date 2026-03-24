85027d853d5fa9a0fea8c3acf3bce96c44e88cde
Isaac Alexander <Isaacalexander81@gmail.com>
2026-03-24 01:55:34 +0100

    src/main/java/com/alex/student_management_system/service/impl/TeacherServiceImpl.java

--- src/main/java/com/alex/student_management_system/service/impl/TeacherServiceImpl.java
+++ src/main/java/com/alex/student_management_system/service/impl/TeacherServiceImpl.java
@@ -1,4 +1,45 @@
 package com.alex.student_management_system.service.impl;
 
-public class TeacherServiceImpl {
+import com.alex.student_management_system.entity.Teacher;
+import com.alex.student_management_system.repository.TeacherRepository;
+import com.alex.student_management_system.service.TeacherService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TeacherServiceImpl implements TeacherService {
+
+    private TeacherRepository teacherRepository;
+
+    public TeacherServiceImpl(TeacherRepository teacherRepository) {
+        super();
+        this.teacherRepository = teacherRepository;
+    }
+
+    @Override
+    public List<Teacher> getAllTeachers() {
+        return teacherRepository.findAll(); // returns list of teacher
+    }
+
+    @Override
+    public Teacher saveTeacher(Teacher teacher) {
+        return teacherRepository.save(teacher);
+    }
+
+    @Override
+    public Teacher getTeacherById(Long id) {
+        return teacherRepository.findById(id).get();
+    }
+
+    @Override
+    public Teacher updateTeacher(Teacher teacher) {
+        return teacherRepository.save(teacher);
+    }
+
+    @Override
+    public void deleteTeacherById(Long id) {
+        teacherRepository.deleteById(id);
+    }
+
 }
