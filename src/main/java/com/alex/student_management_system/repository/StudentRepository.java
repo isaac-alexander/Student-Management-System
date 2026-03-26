019b4ebababb2b8e0d93079700a416d0b143082c
Isaac Alexander <Isaacalexander81@gmail.com>
2026-03-26 02:04:37 +0100

    src/main/java/com/alex/student_management_system/repository/StudentRepository.java

--- src/main/java/com/alex/student_management_system/repository/StudentRepository.java
+++ src/main/java/com/alex/student_management_system/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import com.alex.student_management_system.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    boolean existsByEmail(String email);
 }
